ec69dac9545948ec752f7d2d105510b698a3f77a
Sarah Hendricks <slynnhendricks@gmail.com>
2016-04-03 17:31:07 -0400

    Frontpage & css shells

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" />
+<head>
+	<title>Guide to DC Cooking Classes</title>
+	<meta charset="utf-8" />
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	<script></script>
+</head>
+<body>
+	<header>
+		<div id="Logo">
+		</div>
+		<nav>
+			<div id="general-nav">
+				<ul>
+					<div id="float-left">
+						<li><a href="index.php">Home</a></li>
+					</div>
+					<div id="float-right">
+						<li><a href="help.php">About</a></li>
+						<li id="sign-in"><a href="login.php">Sign In</a></li>
+					</div>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+	<div id="content">
+		<a>
+			<div id="jumbo">
+			</div>
+		</a>
+		<div id="welcome">
+		</div>
+		<div id="products-special">
+		</div>
+		<div id="story">
+		</div>
+
+	</div>
+
+	<footer>
+		<ul>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="">Follow</a></li>
+		</ul>
+	</footer>
+</body>
+
+</html>
