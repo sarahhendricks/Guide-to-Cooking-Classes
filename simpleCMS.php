3e7cd59663a61ecd84e4c0a79769e95c6dd0212a
Sarah Hendricks <slynnhendricks@gmail.com>
2016-04-03 18:06:34 -0400

    Begin building CMS functions

---
+++ simpleCMS.php
@@ -0,0 +1,29 @@
+<?php
+
+class simpleCMS {
+	var $host;
+	var $username;
+	var $password;
+	var $table;
+
+	public function display_public() {
+
+	}
+
+	public function display_admin() {
+
+	}
+
+	public function write() {
+
+	}
+
+	public function connect() {
+
+	}
+
+	public function buildDB() {
+		
+	}
+}
+
