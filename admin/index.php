ec6918ada2ac4ccede1416db3855e135246efc7d
Sarah Hendricks <slynnhendricks@gmail.com>
2016-04-07 10:09:14 -0400

    Delete page function

--- admin/index.php
+++ admin/index.php
@@ -7,4 +7,13 @@
 		logout();
 	}
 	
+	//check if a page has been deleted
+	if(isset($_GET['delpage'])) {
+		$delpage = $_GET['delpage'];
+		$delpage = mysql_real_escape_string($delpage);
+		$sql = mysql_query("DELETE FROM pages WHERE pageID = '$delpage'");
+		$_SESSION['success'] = "Page deleted";
+		header('Location: '.DIRADMIN);
+		exit();
+	}
 ?>
