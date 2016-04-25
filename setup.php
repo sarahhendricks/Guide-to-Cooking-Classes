33c4afb0d1346a8a0fd00a8c2d9e468d7787efee
Sarah Hendricks <slynnhendricks@gmail.com>
2016-04-25 10:35:53 -0400

    Add links to setup

--- setup.php
+++ setup.php
@@ -19,14 +19,15 @@ INSERT INTO `members` (`memberID`, `username`, `password`) VALUES
 CREATE TABLE IF NOT EXISTS `pages` (
   `pageID` int(11) NOT NULL AUTO_INCREMENT,
   `pageTitle` varchar(255) DEFAULT NULL,
+  `pageLink` varchar(255) DEFAULT NULL,
   `photo` varchar(255) DEFAULT NULL,
   `pageCont` text,
   PRIMARY KEY (`pageID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
-INSERT INTO `pages` (`pageID`, `pageTitle`, `photo`, `pageCont`) VALUES
-(1, 'Home', 'photos/apple_pie_making.jpeg', '<p>Sample Sample content</p>'),
-(2, 'About', 'photos/carving_beef.jpeg', '<p>Sample Sample content</p>'),
-(3, 'Services', 'photos/food_tray.jpeg', '<p>Sample Sample content</p>'),
-(4, 'News', 'photos/pasta_making.jpeg', '<p>Sample Sample content</p>'),
-(5, 'Contact', 'photos/picnic_and_wine.jpeg', '<p>Sample Sample content</p>');
+INSERT INTO `pages` (`pageID`, `pageTitle`, `pageLink`, `photo`, `pageCont`) VALUES
+(1, 'Home', 'www.google.com', 'photos/apple_pie_making.jpeg', '<p>Sample Sample content</p>'),
+(2, 'About', 'www.google.com', 'photos/carving_beef.jpeg', '<p>Sample Sample content</p>'),
+(3, 'Services', 'www.google.com', 'photos/food_tray.jpeg', '<p>Sample Sample content</p>'),
+(4, 'News', 'www.google.com', 'photos/pasta_making.jpeg', '<p>Sample Sample content</p>'),
+(5, 'Contact', 'www.google.com', 'photos/picnic_and_wine.jpeg', '<p>Sample Sample content</p>');
